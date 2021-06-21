20be2270ae6b88b6ec45dad9e8b55a97c66ada97
Arley Ferreira de Souza <arley.ferreira.souza@gmail.com>
2021-06-20 22:45:31 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Grid, Box, Button } from "@material-ui/core";
 import axios from "axios";
 
 const App = () => {
-  const [botoes, setBotoes] = useState([{nro:1,quant:0},{nro:2,quant:0},{nro:3,quant:0},{nro:4,quant:0},{nro:5,quant:0}])
+  const [registros, setRegistros] = useState([]);
 
   useEffect(() => {
     select();
@@ -14,41 +13,14 @@ const App = () => {
       .get("/select")
       .then((response) => {
         if (response.data.error) alert(response.data.error);
-        else{ 
-          const aux = [...botoes];
-          response.data.rows.forEach(item => {
-            console.log('item.nro:' + item.nro)
-            aux[item.nro-1].quant = item.quant
-          })
-          setBotoes(aux)
-        }
+        else setRegistros(response.data.rows);
       })
       .catch((e) => alert(e.message));
   };
 
-  const salvar = (nro) => {
-    axios
-      .get(`/insert/${nro}`)
-      .then((response) => {
-        if (response.data.error) alert(response.data.message);
-        else if (response.data.rowCount === 1) select();
-      })
-      .catch((e) => console.log(e.message));
-  };
+  const lista = registros.map( item => <div>{item.nro}: {item.quant}</div> )
 
-  const lista = botoes.map( item => 
-    <Box m={1}>
-      <Button variant="contained" color="primary" key={item.nro} onClick={() => salvar(item.nro)}>
-        {item.nro}:{item.quant}
-      </Button>
-    </Box>
-  );
-
-  return (
-    <Grid container style={{backgroundColor:'#dedede'}} justify="center">
-        {lista}
-    </Grid>
-  );
+  return ( <div className="App">  {lista}  </div> );
 }
 
 export default App;
